dfc57d46c2e42594cd358d065dae9b31caf39a39
Mohamadreza Azizi <154751813+rezalwl@users.noreply.github.com>
2024-08-28 08:46:38 +0330

    install react-bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import TourList from './components/tourList'
-import './App.css'
+import "./App.css";
+import Header from "./components/header/header";
+import MainBody from "./components/MainBody/MainBody";
+import Footer from "./components/footer/footer";
 function App() {
   return (
     <>
-      <TourList />
+      <Header />
+      <MainBody />
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MainBody/MainBody.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainBody = () => {
+  return (
+    <div>B</div>
+  )
+}
+
+export default MainBody
